de72270123dbbc4ed04f7647c10d626f94625773
Александр Соколов <sookooliki@outlook.com>
2016-12-23 10:37:14 +0300

    Update getting places types

--- src/main/java/ru/itmo/ipm/repository/arq/PlaceRepository.java
+++ src/main/java/ru/itmo/ipm/repository/arq/PlaceRepository.java
@@ -1,7 +1,6 @@
 package ru.itmo.ipm.repository.arq;
 
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.shared.PrefixMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -28,11 +27,11 @@ public class PlaceRepository implements IPlaceRepository {
     public List<Place> getAll(double lat1, double lon1, double lat2, double lon2, List<PlaceType> types) throws SQLException {
         List<Place> places = new ArrayList<>();
         String queryString =
-                "SELECT ?resourceUrl ?lat ?long ?title ?description ?thumbnail ?type WHERE{\n" +
+                "SELECT ?resourceUrl ?type (AVG(?lat) as ?avgLat) (AVG(?long) as ?avgLong) ?title ?description ?thumbnail WHERE{\n" +
                         "?resourceUrl rdf:type  ?type.\n" +
-                        "FILTER(?type IN (" + types.stream().map(placeType -> "<" + placeType.getResourceUrl() + ">").reduce((s, s2) -> s + ", " + s2).get() + ")) \n" +
                         "?resourceUrl geo:lat ?lat .\n" +
                         "?resourceUrl geo:long ?long .\n" +
+                        "FILTER(?type IN (" + types.stream().map(placeType -> "<" + placeType.getResourceUrl() + ">").reduce((s, s2) -> s + ", " + s2).get() + ")) \n" +
                         "FILTER(\n" +
                         "(?lat > \"" + lat1 + "\"^^xsd:float) && \n" +
                         "(?lat < \"" + lat2 + "\"^^xsd:float) && \n" +
@@ -43,7 +42,7 @@ public class PlaceRepository implements IPlaceRepository {
                         "?resourceUrl rdfs:comment ?description .\n" +
                         "FILTER(LANG(?description) = \"\" || LANG(?description) = \"ru\")\n" +
                         "?resourceUrl dbo:thumbnail ?thumbnail .\n" +
-                        "}";
+                        "} group by ?resourceUrl ?type ?title ?description ?thumbnail \n";
         Query query = QueryFactory.create();
         query.setPrefixMapping(prefixMapping);
         query = QueryFactory.parse(query, queryString, null, null);
@@ -59,8 +58,8 @@ public class PlaceRepository implements IPlaceRepository {
                 place.setThumbnail(solution.getResource("thumbnail").getURI());
 
                 place.setLocation(new Location());
-                place.getLocation().setLatitude(solution.getLiteral("lat").getDouble());
-                place.getLocation().setLongitude(solution.getLiteral("long").getDouble());
+                place.getLocation().setLatitude(solution.getLiteral("avgLat").getDouble());
+                place.getLocation().setLongitude(solution.getLiteral("avgLong").getDouble());
 
                 place.setPlaceType(new PlaceType());
                 place.getPlaceType().setResourceUrl(solution.getResource("type").getURI());
@@ -74,18 +73,16 @@ public class PlaceRepository implements IPlaceRepository {
     @Override
     public List<PlaceType> getAllTypes() {
         List<PlaceType> placeTypes = new ArrayList<>();
-        String queryString = "SELECT DISTINCT ?parentType ?type ?title WHERE {\n" +
+        String queryString = "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>" +
+                "SELECT DISTINCT ?type ?parentType ?title WHERE {\n" +
                 "?type rdfs:subClassOf* <http://dbpedia.org/ontology/ArchitecturalStructure> .\n" +
                 "?type rdfs:subClassOf ?parentType .\n" +
                 "?type rdfs:label ?title .\n" +
                 "FILTER(LANG(?title)=\"\" || LANG(?title)=\"en\" || LANG(?title)=\"ru\")\n" +
                 "}";
-        Query query = QueryFactory.create();
-        query.setPrefixMapping(prefixMapping);
-        query = QueryFactory.parse(query, queryString, null, null);
+        Query query = QueryFactory.create(queryString);
         try (QueryExecution queryExecution = QueryExecutionFactory.sparqlService(SERVICE_ENDPOINT, query)) {
             ResultSet resultSet = queryExecution.execSelect();
-            Model resourceModel = resultSet.getResourceModel();
             for (; resultSet.hasNext(); ) {
                 QuerySolution solution = resultSet.nextSolution();
                 PlaceType placeType = new PlaceType();
