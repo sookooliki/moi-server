8b7fcf1ac9919c294c3ae1a5b28745c30bc717d6
Александр Соколов <sookooliki@outlook.com>
2017-01-22 18:10:39 +0300

    fix problem with swagger ui

--- src/main/java/ru/itmo/ipm/App.java
+++ src/main/java/ru/itmo/ipm/App.java
@@ -1,6 +1,7 @@
 package ru.itmo.ipm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
